9a8fc0fa0f23b2eca96febe182af63076f256516
Brian Stephan <brian.stephan.2440@edmail.edcc.edu>
2016-05-24 16:35:36 -0700

    Added lab11.sh, functionLibrary.sh, arrays.sh, timefiles, timefiles.tar.gz, and functions.sh

---
+++ functionLibrary.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+CheckDirectory()
+{
+
+	if [ ! -d "${DIRECTORY}/${YEAR}/${MONTH}/${DAY}" ] ;then
+        	mkdir -p "${DIRECTORY}/${YEAR}/${MONTH}/${DAY}"
+	fi
+
+}
+
+copyfiles()
+{
+
+        cp $LINE "${DIRECTORY}/${YEAR}/${MONTH}/${DAY}"
+
+}
+
+movefiles()
+{
+
+	 mv $LINE "${DIRECTORY}/${YEAR}/${MONTH}/${DAY}"
+
+}
+
+GetTimeStamp()
+{
+
+		TMP=$(stat --format '%y' "$LINE")
+                TMP=${TMP%%' '*}
+                YEAR=${TMP%%-*}
+                TMP=${TMP#*-}
+                MONTH=${TMP%%-*}
+                TMP=${TMP#*-}
+                DAY=${TMP%%-*}
+
+}
