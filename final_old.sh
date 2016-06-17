daee76e48fb38aac66a5eb2f5d5dd997c332c37b
Brian Stephan <brian.stephan.2440@edmail.edcc.edu>
2016-06-16 22:22:39 -0700

    Added final_old.sh and library_old.sh

---
+++ final_old.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+trap "NoSorting" SIGINT
+
+. ~/Projects/CS225/library.sh
+
+declare -a YEAR MONTH DAY
+
+if_Exif
+
+if_Exiftool
+
+rm -Rf ~/Projects/CS225/finalfiles/2016
+
+DIRECTORY=~/Projects/CS225/finalfiles
+
+LSDIR=$( find $DIRECTORY )
+
+#FILELINE=$(stat --format '%y')
+
+TMP=$(mktemp)
+#TMP1=$(mktemp)
+#TMP2=$(mktemp)
+#TMP3=$(mktemp -d)
+
+while getopts :fdth opt ;do
+
+   case $opt in
+        f)FORCE="true" ;;
+	d)set -x ;;
+	t)TARUNZIP ;;
+        h) echo "Your options in $0 are:
+         -f Force is enable/true
+	 -d Turns on debugging
+	 -t Unzips finalfiles.tar.gz for txt files
+         -h Help " ;;
+         \?) echo "Unknown option" ;;
+   esac
+done
+shift $(($OPTIND-1))
+
+for LINE in $LSDIR ;do
+
+	if [ ! -d $LINE ] ;then
+
+		GetTimeStamp
+		CheckDirectory
+
+		if [ -z $FORCE ] ;then
+
+			copyfiles 
+		#	CheckCameraMakeModelDT
+		else
+
+			movefiles
+		#	CheckCameraMakeModelDT
+		fi
+	fi
+CheckCameraMakeModelDT
+done
+
+cleanup
---
+++ library_old.sh
@@ -0,0 +1,120 @@
+#!/bin/bash
+
+# Checking if exiv2 is installed or needs to be installed
+if_Exif()
+{
+
+exiv2 &>/dev/null
+if [[ ${?} = 127 ]] ;then
+
+	logger "Installing Exif (exiv2) from ${0} script."
+	yum -y install exiv2 &>~/Projects/CS225/installexiv2.log
+	echo "Exif (exiv2) is installed!"
+	
+else 
+	logger "Exif (exiv2) is already installed"
+	echo "Exif (exiv2) is already installed"
+fi
+
+} &>~/Projects/CS225/exiv2.log
+
+
+# Checking if exiftool is installed or needs to be installed
+if_Exiftool()
+{
+
+exiftool &>/dev/null
+if [[ ${?} = 127 ]] ;then
+
+	logger "Installing Exiftool from ${0} script."
+	yum -y install perl-ExtUtils-MakeMaker &>~/Projects/CS225/perl-ExtUtils.log
+	# Using temp dir to download and unzip program files
+	# Got this idea from http://www.sno.phy.queensu.ca/~phil/exiftool/install.html
+	TMP3=$(mktemp -d)
+	cd ${TMP3}
+	wget http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-10.20.tar.gz
+	gzip -dc Image-ExifTool-10.20.tar.gz | tar -xf -
+	cd Image-ExifTool-10.20/
+	perl Makefile.PL
+    	make test
+    	make install
+	cd
+	rm -Rf ${TMP3}
+else
+	
+	logger "Exiftool is already installed."
+	echo "Exiftool is already installed."
+
+fi	
+} &>~/Projects/CS225/exiftool.log 
+
+
+CheckDirectory()
+{
+
+	if [ ! -d "${DIRECTORY}/${YEAR}/${MONTH}/${DAY}" ] ;then
+        	mkdir -p "${DIRECTORY}/${YEAR}/${MONTH}/${DAY}"
+	fi
+
+}
+
+copyfiles()
+{
+
+        cp $LINE "${DIRECTORY}/${YEAR}/${MONTH}/${DAY}"
+
+}
+
+movefiles()
+{
+
+	 mv $LINE "${DIRECTORY}/${YEAR}/${MONTH}/${DAY}"
+
+}
+
+GetTimeStamp()
+{
+
+		TMP=$(stat --format '%y' "$LINE")
+                TMP=${TMP%%' '*}
+                YEAR=${TMP%%-*}
+                TMP=${TMP#*-}
+                MONTH=${TMP%%-*}
+                TMP=${TMP#*-}
+                DAY=${TMP%%-*}
+
+}
+
+#2014-8-24_8-30-16_Canon-S80_2.jpg
+CheckCameraMakeModelDT()
+{
+ 
+	exiftool '-FileName<CreateDate' -d %Y-%m-%d_%H-%M-%S_${model;}%%_c.%%e ${DIRECTORY}/${YEAR}/${DAY} 
+
+}
+
+NoSorting()
+{
+	echo "Media files sorting stopped"
+	cleanup
+}
+
+cleanup()
+{
+        # removes TMP files
+
+        rm -Rf $TMP
+	rm -Rf $TMP1
+	rm -Rf $TMP2
+        echo "Cleaned up TMP files"
+
+        exit
+}
+
+
+TARUNZIP()
+{
+        cd /root/Projects/CS225
+        tar -xzf /root/Projects/CS225/finalfiles.tar.gz
+}
+
