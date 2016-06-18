3cf756cf10fd06b3d85c25e035cb2d507c80df6c
Brian Stephan <brian.stephan.2440@edmail.edcc.edu>
2016-06-18 00:07:42 -0700

    Modified and rewritten final.sh and library.sh. It works
    Added SIGINT and SIGTERM traps

--- final.sh
+++ final.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
+# Brian Stephan
+# 6/17/2016
+# final.sh
 
-trap "NoSorting" SIGINT
+
+#trap "TRAP_C" SIGINT
+
+trap "TRAP_T" SIGTERM
+
+trap "trapCleanup" SIGINT SIGTERM
+
+#trap "Error" INT TERM
 
 . ~/Projects/CS225/library.sh
 
-if_Exif
+# Checking if packages are installed:
+echo -e "Checking if all necessary packages are installed. Please wait as this may take up to a minute."
 
-if_Exiftool
+if_exiv2 INSTALL
 
-rm -Rf ~/Projects/CS225/finalfiles/2016
+echo "Exiv2 is installed!"
 
-declare -a YEAR MONTH DAY
+if_exiftool INSTALL
+
+echo "Exiftool is installed!"
+
+
+#rm -Rf ~/Projects/CS225/finalfiles/2016
+
+#declare -a YEAR MONTH DAY
 
 DIRECTORY=~/Projects/CS225/finalfiles
 
 LSDIR=$( find $DIRECTORY )
 
 #FILELINE=$(stat --format '%y')
-
-TMP=$(mktemp)
-TMP1=$(mktemp)
-TMP2=$(mktemp)
-TMP3=$(mktemp)
-TMP4=$(mktemp)
-#TMP5=$(mktemp -d)
-TMP6=$(mktemp)
-TMP7=$(mktemp)
 
 while getopts :fdth opt ;do
 
@@ -36,31 +45,94 @@ while getopts :fdth opt ;do
         h) echo "Your options in $0 are:
          -f Force is enable/true
 	 -d Turns on debugging
-	 -t Unzips finalfiles.tar.gz for txt files
+	 -t Unzips finalfiles.tar.gz for image and video files
          -h Help " ;;
          \?) echo "Unknown option" ;;
    esac
 done
 shift $(($OPTIND-1))
 
-for LINE in $LSDIR ;do
+PictureCounter=0
+VideoCounter=0
+PictureDuplicate=0
+VideoDuplicate=0
+Sort=0
+FileCount=0
 
-	if [ ! -d $LINE ] ;then
+TMPS=$(mktemp /root/Projects/CS225/tmps.XXXXXX)
 
-		GetTimeStamp
-		CheckDirectory
+#TMP=$(mktemp)
+#TMP1=$(mktemp)
+#TMP2=$(mktemp)
+#TMP3=$(mktemp)
+#TMP4=$(mktemp)
+#TMP5=$(mktemp -d)
+#TMP6=$(mktemp)
+#TMP7=$(mktemp)
 
-		if [ -z $FORCE ] ;then
+for FILE in $LSDIR ;do
+  
+	(( FileCount ++ ))
 
-			copyfiles 
-		#	CheckCameraMakeModelDT
-		else
-
-			movefiles
-		#	CheckCameraMakeModelDT
-		fi
-	fi
-CheckCameraMakeModelDT
 done
 
+
+for FILE in $LSDIR ;do
+  
+if [[ "$FILE" == *.JPG ]] ;then
+
+	EXIV2 ${FILE}
+else
+
+	TIME ${FILE}
+fi
+
+	CreateDirectory ${FILE}
+  	
+	(( Sort++ ))
+
+if [[ ! -e $DEST ]] ;then
+   
+	FilesCopyingMoving
+else
+	TMP_MD5SUM
+    
+if [[ "$Duplicate" -ne 1 ]] ;then
+     
+	FilesCopyingMoving
+	
+	fi
+fi
+	Duplicate=""
+
+done
+
+Summary
+
 cleanup
+
+
+
+
+
+#for LINE in $LSDIR ;do
+
+#	if [ ! -d $LINE ] ;then
+
+#		GetTimeStamp
+#		CheckDirectory
+
+#		if [ -z $FORCE ] ;then
+
+#			copyfiles 
+		#	CheckCameraMakeModelDT
+#		else
+
+#			movefiles
+		#	CheckCameraMakeModelDT
+#		fi
+#	fi
+#CheckCameraMakeModelDT
+#done
+
+#cleanup
--- library.sh
+++ library.sh
@@ -1,151 +1,440 @@
 #!/bin/bash
+# Brian Stephan
+# library.sh
 
 # Checking if exiv2 is installed or needs to be installed
-if_Exif()
+
+# Function to install/remove exiv2:
+
+if_exiv2()
 {
 
-exiv2 &>/dev/null
-if [[ ${?} = 127 ]] ;then
+# Installing the exiv2
 
-	logger "Installing Exif (exiv2) from ${0} script."
-	yum -y install exiv2 &>~/Projects/CS225/installexiv2.log
-	echo "Exif (exiv2) is installed!"
+if [ ${1} == INSTALL ] ;then
+
+	exiv2 &>/dev/null
 	
-else 
-	logger "Exif (exiv2) is already installed"
-	echo "Exif (exiv2) is already installed"
+	if [[ ${?} = 127 ]] ;then
+
+        logger "Installing exiv2 in preparation for running ${0} script."
+       
+	echo "Exiv2 was not found on your system; installing..." | tee ~/Projects/CS225/exiv2.log
+	yum -y install exiv2 &>>~/Projects/CS225/exiv2.log
+        
+	if [[ ${?} = 0 ]] ;then
+
+	echo "The package exiv2 was installed successfully!" | tee -a ~/Projects/CS225/exiv2.log
+        
+	fi
+
+else
+
+	echo "Exiv2 appears to be installed and is working properly, continuing..." | tee -a ~/Projects/CS225/exiv2.log
+    
+	fi
+
 fi
 
-} &>~/Projects/CS225/exiv2.log
+# Removing the exiv2:
+
+if [ ${1} == REMOVE ] ;then
+    
+	logger "Removing the exiv2 using the ${0} script."
+	
+	echo "Removing the exiv2 package..." | tee -a ~/Projects/CS225/exiv2.log
+	
+	yum -y remove exiv2 &>>~/Projects/CS225/exiv2.log
+
+	if [[ ${?} = 0 ]] ;then
+        
+	echo "The package exiv2 was removed successfully!" | tee -a ~/Projects/CS225/exiv2.log
+
+	
+	fi
+fi
+
+} 2>>~/Projects/CS225/exiv2.log
+
 
 
 # Checking if exiftool is installed or needs to be installed
-if_Exiftool()
+# Function to install/remove exiftool:
+
+if_exiftool()
 {
 
-exiftool &>/dev/null
+# Installing the exiftool
+
+if [ ${1} == INSTALL ] ;then
+
+exiftool ${0} &>/dev/null
+
 if [[ ${?} = 127 ]] ;then
 
-	logger "Installing Exiftool from ${0} script."
-	yum -y install perl-ExtUtils-MakeMaker &>~/Projects/CS225/perl-ExtUtils.log
-	# Using temp dir to download and unzip program files
-	# Got this idea from http://www.sno.phy.queensu.ca/~phil/exiftool/install.html
-	TMP4=$(mktemp -d)
-	cd ${TMP5}
-	wget http://www.sno.phy.queensu.ca/~phil/exiftool/Image-ExifTool-10.20.tar.gz
-	gzip -dc Image-ExifTool-10.20.tar.gz | tar -xf -
-	cd Image-ExifTool-10.20/
-	perl Makefile.PL
-    	make test
-    	make install
-	cd
-	rm -Rf ${TMP5}
-else
-	
-	logger "Exiftool is already installed."
-	echo "Exiftool is already installed."
+	logger "Installing exiftool in preparation for running ${0} script."
+	echo "ExifTool was not found on your system; installing..." | tee ~/Projects/CS225/exiftool.log
+        
+	# Installing required package perl-ExtUtils-MakeMaker:
+	if [[ ! $(rpm -q perl-ExtUtils-MakeMaker) = 0 ]] ;then
 
-fi	
-} &>~/Projects/CS225/exiftool.log 
+	echo "Installing required package perl-ExtUtils-MakeMaker..." | tee ~/Projects/CS225/perl-ExtUtils.log
+	yum -y install perl-ExtUtils-MakeMaker &>>~/Projects/CS225/perl-ExtUtils.log
 
-
-CheckDirectory()
-{
-
-	if [ ! -d "${DIRECTORY}/${YEAR}/${MONTH}/${DAY}" ] ;then
-        	mkdir -p "${DIRECTORY}/${YEAR}/${MONTH}/${DAY}"
 	fi
 
-}
+	# Using temp dir to download and unzip the program files
+	# Got this idea from http://www.sno.phy.queensu.ca/~phil/exiftool/install.html
+	# Installing exiftool from source: http://www.sno.phy.queensu.ca/~phil/exiftool/
+	# The following installs a specific version of Image-ExifTool.
+	# If the command fails then it will verify if the program version haven't changed.
+	
+	ExifVer=Image-ExifTool-10.20
+	TMP1=$(mktemp -d)
+	cd ${TMP1}
 
-copyfiles()
-{
+	echo "Downloading ExivTool installation files..." | tee -a ~/Projects/CS225/exiftool.log
+	wget http://www.sno.phy.queensu.ca/~phil/exiftool/${ExifVer}.tar.gz &>>~/Projects/CS225/exiftool.log
+	
+	echo "Extracting ExivTool installation files..." | tee -a ~/Projects/CS225/exiftool.log
+        gzip -dc ${ExifVer}.tar.gz | tar -xf - &>>~/Projects/CS225/exiftool.log
+        cd ${ExifVer}
 
-        cp $LINE "${DIRECTORY}/${YEAR}/${MONTH}/${DAY}"
+	perl Makefile.PL &>>~/Projects/CS225/exiftool.log
+	
+	echo "Performing internal tests before installation (It shouldn't take long)..." | tee -a ~/Projects/CS225/exiftool.log
+        
+	make test &>>~/Projects/CS225/exiftool.log
+        
+	if [ ! $? == 0 ] ;then
+	
+		echo "The tests were not successful. Please review exiftool.log for errors."
 
-}
+		else
 
-movefiles()
-{
+		echo "The tests comleted successfully!"
+        
+	fi
 
-	 mv $LINE "${DIRECTORY}/${YEAR}/${MONTH}/${DAY}"
+	echo "Continuing with ExifTool installation..."
+        
+	make install &>>~/Projects/CS225/exiftool.log
 
-}
+	if [[ ${?} = 0 ]] ;then
 
-GetTimeStamp()
-{
+		echo "ExifTool was installed successfully!"
+        
+	fi
+	
+	else
+        
+		echo "ExifTool appears to be installed and is working properly, continuing..." | tee -a ~/Projects/CS225/exiftool.log
+		fi
 
-		TMP=$(stat --format '%y' "$LINE")
-                TMP=${TMP%%' '*}
-                YEAR=${TMP%%-*}
-                TMP=${TMP#*-}
-                MONTH=${TMP%%-*}
-                TMP=${TMP#*-}
-                DAY=${TMP%%-*}
+	fi
+    	
+# Removing the exiftool:
 
-}
+if [ ${1} == REMOVE ] ;then
+
+	logger "Removing the exiftool using the ${0} script."
+    
+	echo "Removing ExifTool..." | tee -a ~/Projects/CS225/exiftool.log
+	
+	UNINSTALL=$(mktemp)
+	cd ${TMP1}/${ExifVer}/
+	
+	make uninstall | grep -i unlink > ${UNINSTALL}
+    
+	. ${UNINSTALL}
+	
+	cd ~
+	
+	rm -rf ${TMP1}
+	rm -f ${UNINSTALL}
+fi
+
+} 2>>~/Projects/CS225/exiftool.log
+
+
+#CheckDirectory()
+#{
+
+#	if [ ! -d "${DIRECTORY}/${YEAR}/${MONTH}/${DAY}" ] ;then
+#        	mkdir -p "${DIRECTORY}/${YEAR}/${MONTH}/${DAY}"
+#	fi
+
+#}
+
+#copyfiles()
+#{
+
+#        cp $LINE "${DIRECTORY}/${YEAR}/${MONTH}/${DAY}"
+
+#}
+
+#movefiles()
+#{
+
+#	 mv $LINE "${DIRECTORY}/${YEAR}/${MONTH}/${DAY}"
+
+#}
+
+#GetTimeStamp()
+#{
+
+#		TMP=$(stat --format '%y' "$LINE")
+#                TMP=${TMP%%' '*}
+#                YEAR=${TMP%%-*}
+#                TMP=${TMP#*-}
+#                MONTH=${TMP%%-*}
+#                TMP=${TMP#*-}
+#                DAY=${TMP%%-*}
+
+#}
 
 #2014-8-24_8-30-16_Canon-S80_2.jpg
-CheckCameraMakeModelDT()
+#CheckCameraMakeModelDT()
+#{
+#TMP1=${File_Name}
+#TMP2=${Create_Date}
+#TMP3=${Camera_Model}
+#TMP4=${Camera_Make}
+
+#exiftool -t | grep File | awk '{ print $3 }' | grep .jpg =$File_Name
+#exiftool -t | grep Create |  awk '{ print $3,$4 }' =$Create_Date
+#exiftool -t | grep Camera | awk '/Name/{ print $4 }' =$Camera_Model
+#exiftool -t | grep Make | awk '{ print $2 }' =$Camera_Make
+
+#TMP6=${Video_File_Name}
+#TMP7=${Video_Date_Time}
+
+#exiftool -t | grep File | awk '{ print $3 }' | grep '.AVI\|.mts' =$Video_File_Name
+#exiftool -t | grep Date/Time | awk '/Original/{ print $3,$4 }' | sed 's/......$//' =$Video_Date_Time
+
+#}
+
+CreateDirectory()
 {
-TMP1=${File_Name}
-TMP2=${Create_Date}
-TMP3=${Camera_Model}
-TMP4=${Camera_Make}
 
-exiftool -t | grep File | awk '{ print $3 }' | grep .jpg =$File_Name
-exiftool -t | grep "Create\|^Modification Date$" | awk '{ print $3,$4 }' =$Create_Date
-exiftool -t | grep Camera | awk '/Name/{ print $4 }' =$Camera_Model
-exiftool -t | grep Make | awk '{ print $2 }' =$Camera_Make
+	DestinationDIR="/root/Projects/CS225/finalfiles/$Year/$Month/$Day"
+	mkdir -p $DestinationDIR
+	DEST=$DestinationDIR/$FileName
 
-if [ ! ${model} ] ;then 
-	exiftool '-FileName<${model;} ${CreateDate}' -d %Y-%m-%d_%I-%M-%S%%-c.%%e $DIRECTORY/$YEAR/$MONTH/$DAY
+}
+
+
+EXIV2()
+{
+
+	CameraMake=$(exiv2 "$FILE" | grep -a "make" | cut -f2 -d ":")
+	CameraMake=${CameraMake# }
+	CameraMake=${CameraMake// /-}
+	CameraModel=$(exiv2 "$FILE" | grep -a "model" | cut -f2 -d ":")
+	CameraModel=${CameraModel# }
+	CameraModel=${CameraModel// /-}
+	Year=$(exiv2 "$FILE" | grep -a "timestamp" | cut -f2 -d ":")
+	Year=${Year# }
+	Month=$(exiv2 "$FILE" | grep -a "timestamp" | cut -f3 -d ":")
+	DayHour=$(exiv2 "$FILE" | grep -a "timestamp" | cut -f4 -d ":")
+	Day=${DayHour%% *}
+	Hour=${DayHour#* }
+	Minute=$(exiv2 "$FILE" | grep -a "timestamp" | cut -f5 -d ":")
+	Seconds=$(exiv2 "$FILE" | grep -a "timestamp" | cut -f6 -d ":")
+
+if [[ "$FILE" == *.JPG ]] ;then
+	
+	FileType="jpg"
 
 else
 
-	 exiftool '-FileName<${CreateDate}' -d %Y-%m-%d_%I-%M-%S_UNKNOWN%%-c.%%e $DIRECTORY/$YEAR/$MONTH/$DAY
+	FileType=${FILE#*.}
 
 fi
+
+if [[ "$CameraModel" =~ "$CameraMake" ]] ;then
+
+        Camera="$CameraModel"
+else
+
+        Camera="$CameraMake-$CameraModel"
+
+fi
+
+	FileName="$Year"-"$Month"-"$Day"_"$Hour"-"$Minute"-"$Seconds"_"$Camera".$FileType
+
 }
 
-CheckVideoNameDT()
+
+TIME()
 {
 
- TMP6=${Video_File_Name}
- TMP7=${Video_Date_Time}
+	Camera="Unknown"
+	Year=$(date +%Y -r $FILE)
+	Month=$(date +%m -r $FILE)
+	Day=$(date +%d -r $FILE)
+	Hour=$(date +%H -r $FILE)
+	Minute=$(date +%M -r $FILE)
+	Seconds=$(date +%S -r $FILE)
+	FileType=${FILE#*.}
+	FileName="$Year"-"$Month"-"$Day"_"$Hour"-"$Minute"-"$Seconds"_"$Camera".$FileType
 
- exiftool -t | grep File | awk '{ print $3 }' | grep '.AVI\|.mts' =$Video_File_Name
- exiftool -t | grep Date/Time | awk '/Original/{ print $3,$4 }' | sed 's/......$//' =$Video_Date_Time
+}
 
- if [ ! ${model} ] ;then
-         exiftool '-FileName<${model;} ${CreateDate}' -d %Y-%m-%d_%I-%M-%S%%-c.%%e $DIRECTORY/$YEAR/$MONTH/$DAY
+TMP_MD5SUM()
+{
+
+	md5=$(md5sum $FILE)
+	md5=${md5%% *}
+
+if grep "$md5" "$TMPS" ;then
+  
+	Duplicate=1
+
+	if [[ "$DEST" == *.jpg ]] ;then
+    	
+	(( PictureDuplicate++ ))
+	
+	else
+    	
+	(( VideoDuplicate++ ))
+
+	fi
 
 else
 
-	 exiftool '-FileName<${CreateDate}' -d %Y-%m-%d_%I-%M-%S_UNKNOWN%%-c.%%e $DIRECTORY/$YEAR/$MONTH/$DAY
+  	(( PostFix++ ))
 
- fi
+	FileName="$Year"-"$Month"-"$Day"_"$Hour"-"$Minute"-"$Seconds"_"$Camera"-"$PostFix".$FileType
+  
+	DEST=$DestinationDIR/$FileName
+
+fi
 
 }
 
-NoSorting()
+FilesCopyingMoving()
 {
-	echo "Media files sorting stopped"
+
+	echo $(md5sum $FILE) >> $TMPS
+
+if [[ "$DEST" == *.jpg ]] ;then
+  	
+	(( PictureCounter++ ))
+
+else
+
+  	(( VideoCounter++ ))
+
+fi
+
+if [[ ! -z "$FORCE" ]] ;then
+	
+	mv $FILE $DEST
+
+	echo "Moved $Sort of $FileCount - $FILE to $DEST"
+
+else
+
+	cp $FILE $DEST
+
+	echo "Copied $Sort of $FileCount - $FILE to $DEST"
+fi
+
+}
+
+Summary()
+{
+
+if [[ ! -z "$FORCE" ]] ;then
+ 
+	ITEM="moved"
+
+else
+
+	ITEM="copied"
+
+fi
+
+echo "$PictureCounter JPEGs $ITEM"
+echo "$PictureDuplicate duplicate JPEGs not $ITEM"
+echo "$VideoCounter videos $ITEM"
+echo "$VideoDuplicate duplicate videos not $ITEM"
+
+}
+
+#NoSorting()
+#{
+#	echo "Media files sorting stopped"
+	
+#	cleanup
+
+#	
+#}
+
+#TRAP_C()
+#{
+
+#	echo "   Please wait for the script to finish renaming/copying/moving!"
+
+#}
+
+TRAP_T()
+{
+
+	echo "   OK, Reverting the changes, please wait..."
+
+	if_Exif REMOVE
+
+	if_Exiftool REMOVE
+
 	cleanup
+
+	exit 1
+
 }
 
 cleanup()
 {
         # removes TMP files
 
-        rm -Rf $TMP
-	rm -Rf $TMP1
-	rm -Rf $TMP2
-	rm -Rf $TMP3
-        echo "Cleaned up TMP files"
+#       rm -Rf $TMP
+#	rm -Rf $TMP1
+#	rm -Rf $TMP2
+#	rm -Rf $TMP3
+#	rm -Rf $TMP4
+#	rm -RF $TMP5
+#	rm -Rf $TMP6
+#	rm -Rf $TMP7
 
-        exit
+	rm -Rf $TMPS
+
+	echo "Cleaned up TMP files"
+
 }
+
+trapCleanup()
+{
+	
+	echo "   You didn't wait for the script to finish renaming/copying/moving!"
+	
+	echo "   Clean up in progress....."
+
+	rm -Rf $TMPS
+
+	echo "   Cleaned up TMP files"
+
+	exit 1
+
+}
+
+
+#Error()
+#{
+
+#	logger "ctrl+c"
+
+#}
 
 
 TARUNZIP()
